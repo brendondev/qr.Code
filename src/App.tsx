3e4813a1f763c7afcba6e96bd7a54d4ac639e084
Brendon Lima <47898677+brendondev@users.noreply.github.com>
2024-09-26 12:53:05 -0300

    analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import type React from "react";
 import "./App.css";
 import QRCodeGenerator from "./components/QRCodeGenerator";
+import { Analytics } from "@vercel/analytics/react";
 
 const App: React.FC = () => {
 	return (
 		<div className="App">
 			<QRCodeGenerator />
+			<Analytics />
 		</div>
 	);
 };
